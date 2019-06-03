74d4300acbecb51eea81569d020ccd5c8cfd0c3b
evepsmith15 <50637882+evepsmith15@users.noreply.github.com>
2019-06-02 22:58:46 -0700

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,52 @@
 
 // Whenever a key is pressed, alert "pressed a button".
-var userText = document.getElementById("userChoice");
+var userText = document.getElementById('userChoice');
 var winText = document.getElementById('win');
-var lossText = document.getElementById('losses');
+var lossesText = document.getElementById('losses');
+var guessesText = document.getElementById('guesses');
 
 //Variable list
 var CPUAnswer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var win = 0;
-var losses = 0;
-var guesses = 9;
-var options = [];
-var letters = [];
+var win = 0; //amount of times you win
+var losses = 0; //amount of times you lost
+var guesses = 9; //the total amount of guesses you have to win
+var options = []; //what the computer has
+var letters = []; //what you already guessed 
 
-var options = CPUAnswer[Math.floor(Math.random() * CPUAnswer.length)];
-options.push(options);
-console.log(options[0]);
-
+var CPUGuess = CPUAnswer[Math.floor(Math.random() * CPUAnswer.length)]; //randomly picks a letter from array
+options.push(CPUGuess); //pushes the answer here. 
 
 //Game starts here 
 document.onkeyup = function(event) {
-var userChoice = event.key;
-letters.push(userChoice)
-console.log(options[0]);
+    var userChoice = event.key;
+    var actualAnswer = String.fromCharCode(userChoice); 
+    letters.push(userChoice);
 
-//win condition
-if (userText === CPUAnswer) {
-    wins++;
-    guesses = 9;
-    options = [];
+    //win condition
+    if (userChoice === CPUGuess) {
+        win++;
+        guesses = 9;
+        letters = [];
+        options = [];
+        CPUGuess = CPUAnswer[Math.floor(Math.random() * CPUAnswer.length)];
+        letters.push(options);
     }
-else if ((userText != CPUAnswer) && (guesses > 0)) {
-    guesses--;
-    options.push(userText);
-    }
-//lose condition
-if (guesses === 0) {
-    loses++;
-    guesses = 9;
-    letters = [];
-    options = [];
-    console.log(options[0]);
-
+    else if ((userChoice != CPUGuess) && (guesses > 0)) {
+        guesses--;
+        options.push(userText);
+        }
+    //lose condition
+    else if (guesses === 0) {
+        losses++;
+        guesses = 9;
+        letters = [];
+        options = [];
+        CPUGuess = CPUAnswer[Math.floor(Math.random() * CPUAnswer.length)];
+        letters.push(options);
 }
 
 userText.textContent = userChoice;
 winText.textContent = win;
-lossText.textContent = losses;
+lossesText.textContent = losses;
+guessesText.textContent = guesses;
 }
